2094a062a820d830e47ee68b702c5e73e5765915
Peter Forret <peter@forret.com>
2020-08-27 16:05:44 +0200

    first working version

--- tests/EstimatorTest.php
+++ tests/EstimatorTest.php
@@ -1,14 +1,87 @@
 <?php
 
-namespace Pforret\Tests;
+namespace Pforret\Estimator\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Pforret\Estimator\Estimator;
 
-class ExampleTest extends TestCase
+class EstimatorTest extends TestCase
 {
     /** @test */
-    public function true_is_true()
+    public function test_initiate()
     {
-        $this->assertTrue(true);
+        $est=New Estimator();
+        $this->assertTrue(isset($est));
     }
+
+    public function test_evaluation()
+    {
+        $est=New Estimator();
+        $averages=[
+            "A" =>  25,
+            "B" =>  25,
+            "C" =>  25,
+            "D" =>  25,
+        ];
+        $est->set_averages($averages);
+        unset($averages["D"]);
+        $evaluation=$est->evaluate_partials($averages);
+        $this->assertTrue($evaluation["count_fraction"]=== .75,"count_fraction is correct");
+        $this->assertTrue($evaluation["total_fraction"]=== .75,"total_fraction is correct");
+    }
+
+    public function test_from_partials()
+    {
+        $est=New Estimator();
+        $averages=[
+            "A" =>  25,
+            "B" =>  25,
+            "C" =>  25,
+            "D" =>  25,
+        ];
+        $est->set_averages($averages);
+        unset($averages["D"]);
+        $estimated=$est->estimate_from_partials($averages);
+        $this->assertEquals(25,$estimated["D"],"Precision 1");
+    }
+
+    public function test_from_partials2()
+    {
+        $est=New Estimator();
+        $averages=[
+            "A" =>  25,
+            "B" =>  25,
+            "C" =>  25,
+            "D" =>  25,
+        ];
+        $est->set_averages($averages);
+        $averages=[
+            "A" =>  250,
+            "B" =>  250,
+            "C" =>  250,
+        ];
+        $estimated=$est->estimate_from_partials($averages,1);
+        $this->assertEquals(250,$estimated["D"],"estimate_from_partials - precision 1");
+
+        $estimated=$est->estimate_from_partials($averages,4);
+        $this->assertEquals(252,$estimated["D"],"estimate_from_partials - precision 4");
+    }
+
+    public function test_from_total()
+    {
+        $est=New Estimator();
+        $averages=[
+            "A" =>  1,
+            "B" =>  2,
+            "C" =>  3,
+            "D" =>  4,
+        ];
+        $est->set_averages($averages);
+        $estimated=$est->estimate_from_total(100,1);
+        $this->assertEquals(40,$estimated["D"],"estimate_from_total - precision 1");
+
+        $estimated=$est->estimate_from_total(100,3);
+        $this->assertEquals(39,$estimated["D"],"estimate_from_total - precision 3");
+    }
+
 }
